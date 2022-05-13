9962608d85188357b9be8a9cd4a2e0014c4cb1de
Chinganshih <anna0935537079@gmail.com>
2022-05-13 14:21:40 -0400

    add declaration

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+/*********************************************************************************
+ *  WEB322 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ * 
+ *  Name: __chinganshih________ Student ID: __148221195__ Date: ___2022-05-13___
+ *
+ *  Heroku Web App URL: ___https://boiling-falls-99685.herokuapp.com_____
+ *
+ *  GitHub Repository URL: __https://github.com/Chinganshih/web322-app.git____
+ *
+ ********************************************************************************/
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
